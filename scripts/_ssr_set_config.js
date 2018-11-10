71ddbbe478b4685dcadc5eed8ada1420ca9567fd
kanghongyan <khongyan379@sina.com>
2018-11-10 11:44:11 +0800

    fix: update ssr-deploy

--- scripts/_ssr_set_config.js
+++ scripts/_ssr_set_config.js
@@ -3,29 +3,36 @@ const { resolve, join } = require('path');
 
 
 const setConfig = async (env) => {
-    if (env === 'development') {
-        await cp(
-            resolve('config/index.js'),
-            resolve('_server/def.js'),
-            {overwrite: true}
-        )
-    }
 
-    if (env === 'test') {
-        await cp(
-            resolve('offline/index.js'),
-            resolve('_server/def.js'),
-            {overwrite: true}
-        )
-    }
 
-    if (env === 'production') {
-        await cp(
-            resolve('online/index.js'),
-            resolve('_server/def.js'),
-            {overwrite: true}
-        )
-    }
+    await cp(
+        resolve('config/index.js'),
+        resolve('_server/def.js'),
+        {overwrite: true}
+    )
+    // if (env === 'development') {
+    //     await cp(
+    //         resolve('config/index.js'),
+    //         resolve('_server/def.js'),
+    //         {overwrite: true}
+    //     )
+    // }
+    //
+    // if (env === 'test') {
+    //     await cp(
+    //         resolve('offline/index.js'),
+    //         resolve('_server/def.js'),
+    //         {overwrite: true}
+    //     )
+    // }
+    //
+    // if (env === 'production') {
+    //     await cp(
+    //         resolve('online/index.js'),
+    //         resolve('_server/def.js'),
+    //         {overwrite: true}
+    //     )
+    // }
 };
 
 module.exports = setConfig
